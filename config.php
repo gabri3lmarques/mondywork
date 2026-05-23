3c481cf611fc37ed92efef9e7b854d190eadc837
gabri3lmarques <gabri3lmarques@gmail.com>
2026-05-23 15:46:19 -0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host'           => 'localhost',
-    'db'             => 'u894432128_tatrampando',
-    'user'           => 'u894432128_tatrampandp_us',
-    'pass'           => 'UR/U|PaC1~2ZfaBg3*',
-    'admin_password' => '#Ky_Nu&*%4@98HS9*7',
-];
-
