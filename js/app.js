9f5041c0d82cf82a236841067de6b277c9bb615d
gabri3lmarques <gabri3lmarques@gmail.com>
2026-07-12 08:02:33 -0300

    ajustando adds

--- js/app.js
+++ js/app.js
@@ -262,9 +262,15 @@
         if (cardsRendered % 2 === 0) {
             var adDiv = document.createElement('div');
             adDiv.className = 'adds-entre-jobs';
-            adDiv.innerHTML =
-                '<scr' + 'ipt>atOptions={"key":"4ebf9218fca889c045c30ab6b62d4769","format":"iframe","height":50,"width":320,"params":{}};</scr' + 'ipt>' +
-                '<scr' + 'ipt src="https://www.highperformanceformat.com/4ebf9218fca889c045c30ab6b62d4769/invoke.js"></scr' + 'ipt>';
+
+            var s1 = document.createElement('script');
+            s1.textContent = 'atOptions={"key":"4ebf9218fca889c045c30ab6b62d4769","format":"iframe","height":50,"width":320,"params":{}}';
+            adDiv.appendChild(s1);
+
+            var s2 = document.createElement('script');
+            s2.src = 'https://www.highperformanceformat.com/4ebf9218fca889c045c30ab6b62d4769/invoke.js';
+            adDiv.appendChild(s2);
+
             container.appendChild(adDiv);
         }
     }
